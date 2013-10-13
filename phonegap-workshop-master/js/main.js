cc7df0287a845518d3b65fde45878349f5fd9268
dudi72 <dudi72@gmail.com>
2013-10-13 22:33:13 +0300

    aaa

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -13,9 +13,20 @@ var app = {
         });
     },
 
+	showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
+	
     initialize: function() {
-        //this.store = new MemoryStore();
-		this.store = new WebSqlStore();
+        this.store = new MemoryStore(function() {
+        self.showAlert('Store Initialized', 'Info');
+		});
+		//this.store = new WebSqlStore();
+		
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
